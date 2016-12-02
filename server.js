e298e2d3bb5f64eb26ad097b51078951037ba765
metincansiper <metincansiper@gmail.com>
2016-12-02 21:03:23 +0300

    Moved index.html to root and some folder renames

--- server.js
+++ server.js
@@ -7,8 +7,4 @@ server.listen(port, function(){
   console.log('server listening on port%d', port);
 });
 
-app.use(express.static(__dirname + '/public'));
-app.use('/node_modules/bootstrap', express.static(__dirname + '/node_modules/bootstrap/'));
-app.use('/node_modules/cytoscape-panzoom', express.static(__dirname + '/node_modules/cytoscape-panzoom/'));
-app.use('/node_modules/qtip2', express.static(__dirname + '/node_modules/qtip2/'));
-app.use('/node_modules/cytoscape-context-menus', express.static(__dirname + '/node_modules/cytoscape-context-menus/'));
+app.use(express.static(__dirname));
